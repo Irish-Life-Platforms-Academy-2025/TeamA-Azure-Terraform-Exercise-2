e1d99a6fd6e5c7a220561b95bd6c7c4ed6149716
[joshpeyton96] <[joshpeyton@hotmail.com]>
2025-02-17 13:59:37 +0000

    Updated files

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "rg" {
   type        = string
-  default     = "josh-nenad-Terraform-rg"
+  default     = "TeamA-rg"
   description = "Name for resource group"
 }
 
@@ -16,15 +16,33 @@ variable "location" {
     default= "eastus"
 }
 
-    variable "private_vnet_cidr" {
-        type = string
-        description = "Private VNet address space (CIDR notation)"
-        default = "10.20.0.0/16"
+variable "password" {
+  type        = string
+  description = "The password for the local account that will be created on the new VM."
+  default     = "P@$$w0rd1234!"
 }
-    variable "public_vnet_cidr" {
-        type = string
-        description = "Public VNet address space (CIDR notation)"
-        default = "10.30.0.0/16"
+
+variable "vm_size" {
+  type = string
+  default = "Standard_D2s_v3"
+}
+
+variable "username" {
+  type        = string
+  description = "The username for the local account that will be created on the new VM."
+  default     = "azureadmin"
+}
+
+variable "public_vnet_cidr" {
+    type = string
+    description = "Private VNet address space (CIDR notation)"
+    default = "10.20.0.0/16"
+}
+
+variable "Private_vnet_cidr" {
+    type = string
+    description = "Public VNet address space (CIDR notation)"
+    default = "10.30.0.0/16"
 }
 
 variable "private-subnet" {
